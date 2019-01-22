5f1368754ff8c068bac3a53456a93da926a525bb
AlbireoImma <faam1612@gmail.com>
2019-01-22 18:24:32 -0300

    Add files via upload

--- vulnerabilidades/kb_search.ps1
+++ vulnerabilidades/kb_search.ps1
@@ -1,16 +1,15 @@
 cls
 
 $vuln = "MS17-010"
-$MS = ".\MS17-010.txt"
+$MS = ".\" + $vuln + ".txt"
 
 [string[]]$servers= Get-Content '..\listado_ip2.txt' # Lista de servidores
 [string[]]$DNS= Get-Content '..\listado_dns2.txt' # Lista de dns
 [string[]]$KBS= Get-Content $MS # Lista de dns
 
-
 $contador = -1
 $ruta = ".\log\$($vuln)_$(Get-Date -Uformat "%d%m%Y%H%M").csv"
-"IP,HOSTNAME,KB_ID,MS,INSTALADO" >> $ruta
+"IP,HOSTNAME,KB_ID,MS,INSTALADO,ERROR" >> $ruta
 
 foreach($server in $servers){
     $contador += 1
@@ -18,38 +17,36 @@ foreach($server in $servers){
     $test = Test-Connection $server -Count 1 -Quiet
     if ($test) {
         write-host [(Get-Date -format g)] $server": Conexion establecida" -foreground "DarkGray"
-        try {
-            $job = start-job Get-Wmiobject -class Win32_QuickFixEngineering -computername $server -AsJob
+        $job = Get-Wmiobject -class Win32_QuickFixEngineering -computername $server -AsJob
+        if($?){
             $estado = ($job | wait-job -Timeout 360).state
             if ($estado -eq "Completed") {
-                $a = Receive-Job -Job $job | where-object {$KBS -contains $_.HotfixID} | Select-Object -property "HotFixID"
-                write-host [(Get-Date -format g)] $a" :Resultado" -foreground "DarkGray"
+                $a = Receive-Job $job | where-object {$KBS -contains $_.HotfixID} | Select-Object -property "HotFixID"
                 if ($a) {
                     write-host [(Get-Date -format g)] $server": Fix Encontrado" -foreground "DarkGray"
-                    $linea = $server + "," + $DNS[$contador] + "," + $a.HotfixID + "," + $vuln + ",Si"
+                    $linea = $server + "," + $DNS[$contador] + "," + $a.HotfixID + "," + $vuln + ",Si,Ninguno"
                     $linea >> $ruta
                 } else {
                     write-host [(Get-Date -format g)] $server": Fix No Encontrado" -foreground "DarkGray"
-                    $linea = $server + "," + $DNS[$contador] + ",," + $vuln + ",No"
+                    $linea = $server + "," + $DNS[$contador] + ",," + $vuln + ",No,Ninguno"
                     $linea >> $ruta
                 }
+                remove-job -state completed
             } else {
-                $job | stop-job
-                $linea = $server + "," + $DNS[$contador] + ",," + $vuln + ",Tiempo Excedido"
+                stop-job $job
+                remove-job -state failed
+                $linea = $server + "," + $DNS[$contador] + ",," + $vuln + ",Desconocido,Tiempo Excedido o error al crear el trabajo"
                 $linea >> $ruta
             }
-            
-        }
-        catch {
+        } else {
             write-host [(Get-Date -format g)] $server": No se pudieron obtener los KB" -foreground "DarkRed"
-            $linea = $server + "," + $DNS[$contador] + ",," + $vuln + ",No se pudieron obtener los KB"
+            $linea = $server + "," + $DNS[$contador] + ",," + $vuln + ",Desconocido,No se pudieron obtener los KB"
             $linea >> $ruta
             write-host [(Get-Date -format g)] $server": IP Analizada" -foreground "DarkGreen"
-            continue
         }
     } else {
         write-host [(Get-Date -format g)] $server": Conexion rechazada" -foreground "DarkRed"
-        $linea = $server + "," + $DNS[$contador] + ",," + $vuln + ",No se pudo establecer conexion"
+        $linea = $server + "," + $DNS[$contador] + ",," + $vuln + ",Desconocido,No se pudo establecer conexion"
         $linea >> $ruta
     }
     write-host [(Get-Date -format g)] $server": IP Analizada" -foreground "DarkGreen"
--- vulnerabilidades/kb_search_2.ps1
+++ vulnerabilidades/kb_search_2.ps1
@@ -1,64 +1,61 @@
 cls
 
 $vuln = "MS17-010"
-$MS = ".\MS17-010.txt"
-
-$Username = 'instctx'
-$Password = 'qwaszx.123'
-$pass = ConvertTo-SecureString -AsPlainText $Password -Force
-$Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
+$MS = ".\" + $vuln + ".txt"
 
 [string[]]$servers= Get-Content '..\listado_ip2.txt' # Lista de servidores
 [string[]]$DNS= Get-Content '..\listado_dns2.txt' # Lista de dns
 [string[]]$KBS= Get-Content $MS # Lista de dns
-$contador = 0
+
+$FLAG = 0
+$contador = -1
 $ruta = ".\log\$($vuln)_$(Get-Date -Uformat "%d%m%Y%H%M").csv"
-"IP,HOSTNAME,KB_ID,MS,INSTALADO" >> $ruta
+"IP,HOSTNAME,KB_ID,MS,INSTALADO,ERROR,VALOR,INCERTIDUMBRE" >> $ruta
 
-$code = {
-    write-host [(Get-Date -format g)] $server": Conexion establecida" -foreground "DarkGray"
-    try {
-        $a = Get-Hotfix -ErrorAction Stop | where-object {$KBS -contains $_.HotfixID} | Select-Object -property "HotFixID"
-        write-host [(Get-Date -format g)] $a" :Resultado" -foreground "DarkGray"
-        if ($a) {
-            write-host [(Get-Date -format g)] $server": Fix Encontrado" -foreground "DarkGray"
-            $linea = $server + "," + $DNS[$contador] + "," + $a.HotfixID + "," + $vuln + ",Si"
-            return $linea
-        } else {
-            write-host [(Get-Date -format g)] $server": Fix No Encontrado" -foreground "DarkGray"
-            $linea = $server + "," + $DNS[$contador] + ",," + $vuln + ",No"
-            return $linea
-        }
-    }
-    catch {
-        write-host [(Get-Date -format g)] $server": No se pudieron obtener los KB" -foreground "DarkRed"
-        $linea = $server + "," + $DNS[$contador] + ",," + $vuln + ",No se pudieron obtener los KB"
-        write-host [(Get-Date -format g)] $server": IP Analizada" -foreground "DarkGreen"
-        return $linea
-    }
-}
-
-
-foreach($server in $servers){
+foreach ($server in $servers) {
+    $contador += 1
     write-host [(Get-Date -format g)] $server": Analizando IP" -foreground "DarkYellow"
     $test = Test-Connection $server -Count 1 -Quiet
     if ($test) {
-        try {
-            $result = Invoke-Command -ScriptBlock $Code -Credential $Cred -ComputerName $server
-            $result >> $ruta
-        }
-        catch {
-            write-host [(Get-Date -format g)] $server": Error Invoke-Command" -foreground "DarkRed"
+        $historial = ""
+        $sesion = [activator]::CreateInstance([type]::GetTypeFromProgID("Microsoft.Update.Session",$server))
+        if ($?) {
+            write-host [(Get-Date -format g)] "Conexion Aceptada" -foreground "DarkGreen"
+            $buscador = $sesion.CreateUpdateSearcher()
+            $registros = $buscador.GetTotalHistoryCount()
+            write-host [(Get-Date -format g)] $registros": Registros" -foreground "DarkGray"
+            $historial = $buscador.QueryHistory(0,$registros) | where-object {2,3 -eq $_.ResultCode} | select-object Title
+            write-host [(Get-Date -format g)] "Query Realizada" -foreground "DarkGreen"
+            foreach ($linea in $historial) {
+                if ($linea -match "(KB\d{4,})") {
+                    if ($KBS -contains $Matches[1]) {
+                        write-host [(Get-Date -format g)] "Fix Encontrado" -foreground "Green"
+                        $line = $server + "," + $DNS[$contador] + "," + $Matches[1] + "," + $vuln + ",Si,Ninguno,1,0"
+                        $line >> $ruta
+                        $FLAG = 1
+                        break
+                    }
+                }
+            }
+            if ($FLAG -eq 0) {
+                write-host [(Get-Date -format g)] "Fix no encontrado" -foreground "DarkRed"
+                $line = $server + "," + $DNS[$contador] + ",," + $vuln + ",No,Ninguno,0,0"
+                $line >> $ruta
+            } else {
+                $FLAG = 0
+            }
+        } else {
+            write-host [(Get-Date -format g)] "Sesion rechazada" -foreground "DarkRed"
+            $linea = $server + "," + $DNS[$contador] + ",," + $vuln + ",Desconocido,No se pudo establecer la sesion,0,1"
+            $linea >> $ruta
         }
     } else {
-        write-host [(Get-Date -format g)] $server": Conexion rechazada" -foreground "DarkRed"
-        $linea = $server + "," + $DNS[$contador] + ",," + $vuln + ",No se pudo establecer conexion"
-        $linea >> $ruta
+        write-host [(Get-Date -format g)] "Conexion rechazada" -foreground "DarkRed"
+            $linea = $server + "," + $DNS[$contador] + ",," + $vuln + ",Desconocido,No se pudo establecer la conexion"
+            $linea >> $ruta
     }
-    write-host [(Get-Date -format g)] $server": IP Analizada" -foreground "DarkGreen"
-    $contador += 1
+    #write-host [(Get-Date -format g)] $server": IP Analizado" -foreground "DarkYellow"
 }
-
 
 
 
